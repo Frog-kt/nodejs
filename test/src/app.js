275e8d9a5a953669e4caa7c946c723d86587c1e0
Frog-kt <kasuo.takumi@gmail.com>
2020-08-14 19:42:41 +0900

    Update app.js

--- test/src/app.js
+++ test/src/app.js
@@ -1,37 +1,73 @@
-const express =require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
-const app=express();
+const app = express();
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const User = new Schema({
-    name : { type: String, require: true, unique: true }, 
-    id   : { type: String, require: true, unique: true },
-    ps : { type: String, require: true }
+
+
+
+// MongoDBの接続情報
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_LOCAL_URI || 'mongodb://localhost:27017/test', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }).then(con => {
+        console.log(`MongoDB Database connected with host: ${con.connection.host}`);
+    })
+};
+
+// MongoDBに接続
+connectDatabase();
+
+// ユーザースキーマの設定
+const userSchema = new mongoose.Schema({
+    name: { type: String, require: true, unique: true },
+    id: { type: String, require: true, unique: true },
+    ps: { type: String, require: true }
 });
-const UserModel = mongoose.model('test', User);
-const data =new UserModel();
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
-app.listen(3000,()=>{console.log('P3000')});
+
+const User = mongoose.model('User', userSchema);
+
+
+app.listen(3000, () => { console.log('P3000') });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.get('/',(req,res)=>{
+
+
+app.get('/', (req, res) => {
     console.log(req.query);
     res.status(200).send('OK');
 });
-app.get('/;id',(req,res)=>{
+
+
+app.get('/;id', (req, res) => {
     console.log(req.params);
     res.status(200).send('OK');
 });
-app.post('/login',(req,res)=>{
+
+
+app.post('/signup', async (req, res) => {
     console.log(req.body);
     console.log(req.body.id);
     console.log(req.body.ps);
     console.log(req.body.name);
-    data.id=req.body.id;
-    data.ps=req.body.ps;
-    data.name=req.body.name;
-    data.save(function(err) {
-        if (err) { console.log(err); }
-    });
+
+    const {
+        id,
+        ps,
+        name
+    } = req.body;
+
+    // ユーザー登録
+    try {
+        const user = await User.create({
+            id,
+            ps,
+            name
+        });
+    } catch (err) {
+        res.status(500).send('DBに保存できませんでした');
+    }
     res.status(200).send('OK');
 });
